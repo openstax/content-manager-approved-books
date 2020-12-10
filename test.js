c8a51898b59a120f606f68de231ee4ffd8e1f8ab
Brenda Nguyen <bn16@rice.edu>
2020-12-09 17:31:38 -0800

    add schema for git book list

--- test.js
+++ test.js
@@ -9,10 +9,24 @@ const booksListPath = path.resolve('./approved-books.json')
 const schemaPath = path.resolve('./schema.json')
 const bookSlugsPath = path.resolve('./book-slugs.json')
 
+const gitBooksListPath = path.resolve('./approved-git-books.json')
+const gitSchemaPath = path.resolve('./git-schema.json')
+
 test('approved-books.json is JSON', t => {
   const listData = fs.readFileSync(booksListPath, { encoding: 'utf8' })
   JSON.parse(listData)
   t.pass()
+})
+
+test('approved-git-books.json matches schema', t => {
+  const listData = fs.readFileSync(gitBooksListPath, { encoding: 'utf8' })
+  const list = JSON.parse(listData)
+  const schemaData = fs.readFileSync(gitSchemaPath, { encoding: 'utf8' })
+  const schema = JSON.parse(schemaData)
+  const validate = new Ajv({ allErrors: true }).compile(schema)
+  const isValid = validate(list)
+  if (!isValid) { console.error(validate.errors) }
+  t.truthy(isValid)
 })
 
 test('approved-books.json matches schema', t => {
