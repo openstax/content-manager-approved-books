5e6566dcc1e726f7e92a7115dd906795865e89f0
Ripal Nathuji <rnathuji@neoteklabs.com>
2021-03-15 17:02:03 -0500

    Update schema and tests

--- test.js
+++ test.js
@@ -18,17 +18,6 @@ test('approved-books.json is JSON', t => {
   t.pass()
 })
 
-test('approved-git-books.json matches schema', t => {
-  const listData = fs.readFileSync(gitBooksListPath, { encoding: 'utf8' })
-  const list = JSON.parse(listData)
-  const schemaData = fs.readFileSync(gitSchemaPath, { encoding: 'utf8' })
-  const schema = JSON.parse(schemaData)
-  const validate = new Ajv({ allErrors: true }).compile(schema)
-  const isValid = validate(list)
-  if (!isValid) { console.error(validate.errors) }
-  t.truthy(isValid)
-})
-
 test('approved-books.json matches schema', t => {
   const listData = fs.readFileSync(booksListPath, { encoding: 'utf8' })
   const list = JSON.parse(listData)
@@ -38,54 +27,4 @@ test('approved-books.json matches schema', t => {
   const isValid = validate(list)
   if (!isValid) { console.error(validate.errors) }
   t.truthy(isValid)
-})
-
-test('approved-books.json has no duplicate entries', t => {
-  const listData = fs.readFileSync(booksListPath, { encoding: 'utf8' })
-  const list = JSON.parse(listData)
-  list.reduce((acc, book) => {
-    if (acc.some(visited => {
-      return (visited.collection_id === book.collection_id
-        && visited.version === book.version
-        && visited.server === book.server
-        && visited.style === book.style)
-    })) {
-      t.fail(`Duplicate entries found for: ${JSON.stringify(book)}`)
-    }
-    acc.push(book)
-    return acc
-  }, [])
-  t.pass()
-})
-
-test('book-slugs.json has no duplicate UUIDs', t => {
-  const listData = fs.readFileSync(bookSlugsPath, { encoding: 'utf8' })
-  const list = JSON.parse(listData)
-  list.reduce((acc, book) => {
-    if (acc.includes(book.uuid)) {
-      t.fail(`Duplicate slugs found for UUID: ${book.uuid}`)
-    }
-    acc.push(book.uuid)
-    return acc
-  }, [])
-  t.pass()
-})
-
-test('all approved books have a slug defined in book-slugs.json', t => {
-  const slugData = fs.readFileSync(bookSlugsPath, { encoding: 'utf8' })
-  const bookData = fs.readFileSync(booksListPath, { encoding: 'utf8' })
-  const books = JSON.parse(bookData)
-  const slugs = JSON.parse(slugData)
-
-  bookToSlugs = slugs.reduce((acc, book) => {
-    acc[book['uuid']] = book['slug']
-    return acc
-  }, {})
-
-  books.forEach(book => {
-    if (!(book['uuid'] in bookToSlugs)) {
-      t.fail(`No slug found for: ${JSON.stringify(book)}`)
-    }
-  });
-  t.pass()
 })
